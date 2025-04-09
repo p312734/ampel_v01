721f890941c045c83652d76f3820a2f70634a49a
Oliver-Balb <34969190+p312734@users.noreply.github.com>
2025-04-09 21:46:25 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,8 @@
+Environment.ledBrightness(AnalogPin.P3, false)
+Environment.ledBrightness(AnalogPin.P4, false)
+Environment.ledBrightness(AnalogPin.P6, false)
+Environment.ledBrightness(AnalogPin.P7, false)
+Environment.ledBrightness(AnalogPin.P9, false)
+basic.forever(function () {
+	
+})
