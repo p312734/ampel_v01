06fc0ec98fbc5891a4fe9d408a086d72746d1c4b
Oliver-Balb <34969190+p312734@users.noreply.github.com>
2025-04-09 22:24:39 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 Environment.ledBrightness(AnalogPin.P3, false)
 Environment.ledBrightness(AnalogPin.P4, false)
-Environment.ledBrightness(AnalogPin.P6, false)
-Environment.ledBrightness(AnalogPin.P7, false)
+Environment.ledBrightness(AnalogPin.P6, true)
+Environment.ledBrightness(AnalogPin.P7, true)
 Environment.ledBrightness(AnalogPin.P9, false)
 basic.forever(function () {
-	
+    if ((0 as any) == (1 as any)) {
+        if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+            music.play(music.tonePlayable(131, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            Environment.ledBrightness(AnalogPin.P3, false)
+            Environment.ledBrightness(AnalogPin.P4, false)
+            Environment.ledBrightness(AnalogPin.P6, true)
+            Environment.ledBrightness(AnalogPin.P7, true)
+            Environment.ledBrightness(AnalogPin.P9, false)
+            if (input.pinIsPressed(TouchPin.P2)) {
+                music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+                Environment.ledBrightness(AnalogPin.P3, true)
+                Environment.ledBrightness(AnalogPin.P4, false)
+                Environment.ledBrightness(AnalogPin.P6, false)
+                Environment.ledBrightness(AnalogPin.P7, false)
+                Environment.ledBrightness(AnalogPin.P9, true)
+            }
+        }
+    }
 })
